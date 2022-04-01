b7cf0a65fb6982a8ec5c45fb5ca2a6825602be8d
Julien WITTOUCK <julien.wittouck@gmail.com>
2022-04-01 14:16:36 +0200

    🔥 : remove old tech-radar code

--- src/site.js
+++
@@ -1,9 +0,0 @@
-require('./common')
-require('./images/radar_legend.png')
-require('./gtm.js')
-
-const CSVDocument = require('./util/factory')
-
-var sheet;
-sheet = CSVDocument()
-sheet.init().build()
